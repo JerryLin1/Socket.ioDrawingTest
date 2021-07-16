3144b22f4b57807a21c37a289709918f83b0dfa8
JerryLin1 <jerreelynn@gmail.com>
2021-07-16 18:31:59 -0400

    comments + refresh on disconnect

--- client/index.js
+++ client/index.js
@@ -1,18 +1,26 @@
 socket = io();
-var isDrawing = false;
 var lastMousePos;
 var linesToDraw = [];
+
+// p5.js function that is called once at start
 function setup() {
     createCanvas(800, 500);
     background(255);
     lastMousePos = createVector(mouseX, mouseY);
 }
+
+// p5.js function that constantly updates
 function draw() {
     if (mouseIsPressed) {
         strokeWeight(2);
+        // Draw line locally
         line(lastMousePos.x, lastMousePos.y, mouseX, mouseY);
+
+        // Emit line to server, which will be broadcasted (emitted to all but the client that sent it)
         socket.emit("line", [lastMousePos.x, lastMousePos.y, mouseX, mouseY]);
     }
+    
+    // Draws all lines sent by the server. Ex. other clients' lines, or lines drawn before you connected
     for (l of linesToDraw) {
         strokeWeight(2);
         line(l[0], l[1], l[2], l[3]);
@@ -20,6 +28,13 @@ function draw() {
     linesToDraw = [];
     lastMousePos.set(mouseX, mouseY);
 }
+
+// When receiving lines from server, push them to linesToDraw so that they will all be drawn in draw()
 socket.on("line", line => {
     linesToDraw.push(line);
+});
+
+// Reload page on disconnect from server
+socket.on("disconnect", () => {
+    location.reload();
 });
--- server.js
+++ server.js
@@ -6,24 +6,31 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 const port = 3000;
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
 
 lines = [];
+
+// Use client folder. Automatically uses index.html
 app.use(express.static(__dirname + '/client'));
+
+// Whenever a client connects
 io.on('connection', socket => {
     console.log(`ID: ${socket.id} has joined.`);
 
-    // Push all lines to new connections so lines are persistent
+    // Push all lines to new connections so lines are persistent (only on initial connection)
     for (l of lines) {
         socket.emit("line", l);
     }
 
+    // When client disconnects
     socket.on('disconnect', () => { /* … */ });
+
+    // When client draws a line
     socket.on('line', line => {
         socket.broadcast.emit('line', line);
         lines.push(line);
         // console.log(line);
     })
-});
-server.listen(port, () => {
-    console.log(`Listening on port ${port}`)
 });
